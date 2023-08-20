e70057960ef88f45cf612e8c4e7029c140088e45
Фёдоров Ренат <homerw223v@mail.ru>
2023-08-20 16:52:47 +0700

    Add requirements.txt

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 import sys
 import re
+from colorama import init, Fore
+
+init(autoreset=True)
 
 commands: str = ("---Чтобы вывести все контакты из справочник введите: 1\n"
                  "---Чтобы добавить новую запись в справочник введите: 2\n"
@@ -17,25 +20,26 @@ def _create_contact_info() -> str:
     :return: str
     """
     patterns: list = [['Введите фамилию: ', r'[a-zA-Zа-яА-ЯёЁ-]+', 'Фамилия должна состоять только из букв'],
-                ['Введите имя: ', r'[a-zA-Zа-яА-ЯёЁ-]+', 'Имя должно состоять только из букв'],
-                ['Введите отчество: ', r'[a-zA-Zа-яА-ЯёЁ-]+', 'Отчество должно состоять только из букв'],
-                ['Введите название организации\n'
-                 '(Название организации необходимо написать без пробелов. '
-                 'Допускается использовать символы - \' \"): ', r'[a-zA-Zа-яА-Я-\'\"]+',
-                 'Неверное название организации'],
-                ['Введите рабочий номер телефона: ', r"[0-9+()-]+",
-                 'Неверный номер телефона. Допускаются символы +-()'],
-                ['Введите личный номер телефона: ', r"[0-9+()-]+", 'Неверный номер телефона. Допускаются символы +-()']]
+                      ['Введите имя: ', r'[a-zA-Zа-яА-ЯёЁ-]+', 'Имя должно состоять только из букв'],
+                      ['Введите отчество: ', r'[a-zA-Zа-яА-ЯёЁ-]+', 'Отчество должно состоять только из букв'],
+                      ['Введите название организации\n'
+                       '(Название организации необходимо написать без пробелов. '
+                       'Допускается использовать символы - \' \"): ', r'[a-zA-Zа-яА-Я-\'\"]+',
+                       'Неверное название организации'],
+                      ['Введите рабочий номер телефона: ', r"[0-9+()-]+",
+                       'Неверный номер телефона. Допускаются символы +-()'],
+                      ['Введите личный номер телефона: ', r"[0-9+()-]+",
+                       'Неверный номер телефона. Допускаются символы +-()']]
     answer: list = []
     for i in range(len(patterns)):
         while True:
-            string: str = input(patterns[i][0]).capitalize()
+            string: str = input(Fore.GREEN + patterns[i][0]).capitalize()
             if re.fullmatch(rf'{patterns[i][1]}', string):
                 answer.append(string)
                 break
             else:
                 os.system("clear||cls")
-                print(patterns[i][2])
+                print(Fore.RED + patterns[i][2])
 
     line: str = f"{answer[0]} {answer[1]} {answer[2]} {answer[3]} {answer[4]} {answer[5]}\n"
     return line
@@ -51,21 +55,21 @@ def show_all_contacts() -> None:
         number: int = 1
         contacts: list = file.readlines()
         pages = len(contacts) // 8 if len(contacts) % 8 == 0 else (len(contacts) // 8) + 1
-        print('Список контактов:'.center(os.get_terminal_size().columns))
+        print(Fore.LIGHTYELLOW_EX + 'Список контактов:'.center(os.get_terminal_size().columns))
         for line in contacts:
-            print(f"{number}: {line.strip()}")
+            print(Fore.LIGHTGREEN_EX + f"{number}: {line.strip()}")
             if number % 8 == 0 and (number // 8) < pages:
-                print(f"Страница {(number // 8)} из {pages}".center(os.get_terminal_size().columns))
-                input('Next page')
+                print(Fore.LIGHTCYAN_EX + f"Страница {(number // 8)} из {pages}".center(os.get_terminal_size().columns))
+                input(Fore.MAGENTA + 'Next page')
                 os.system("clear||cls")
-                print('Список контактов:'.center(os.get_terminal_size().columns))
+                print(Fore.LIGHTYELLOW_EX + 'Список контактов:'.center(os.get_terminal_size().columns))
 
             elif number % 8 == 0 and (number // 8) == pages:
-                print(f"Страница {pages} из {pages}".center(os.get_terminal_size().columns))
-                input('Exit')
+                print(Fore.LIGHTCYAN_EX + f"Страница {pages} из {pages}".center(os.get_terminal_size().columns))
+                input(Fore.MAGENTA + 'Exit')
             elif number % 8 != 0 and (number // 8) + 1 == pages and line == contacts[-1]:
-                print(f"Страница {pages} из {pages}".center(os.get_terminal_size().columns))
-                input('Exit')
+                print(Fore.LIGHTCYAN_EX + f"Страница {pages} из {pages}".center(os.get_terminal_size().columns))
+                input(Fore.MAGENTA + 'Exit')
             number += 1
     os.system("clear||cls")
     telephone_book()
@@ -80,7 +84,7 @@ def add_contact() -> None:
     contact: str = _create_contact_info()
     with open('telephone_book.txt', 'a+', encoding='utf-8') as file:
         file.write(contact)
-    print(separator)
+    print(Fore.LIGHTRED_EX + separator)
     telephone_book()
 
 
@@ -91,10 +95,10 @@ def search_contact() -> None:
     """
     os.system("clear||cls")
     while True:
-        pattern: list = input('Введите характеристики для поиска через пробел: ').split()
+        pattern: list = input(Fore.LIGHTGREEN_EX + 'Введите характеристики для поиска через пробел: ').split()
         if not pattern:
             os.system("clear||cls")
-            print("Не предоставлено ни одной характеристики для поиска")
+            print(Fore.LIGHTRED_EX + "Не предоставлено ни одной характеристики для поиска")
         else:
             break
     count: int = 0
@@ -118,10 +122,10 @@ def change_contact_information() -> None:
     """
     os.system("clear||cls")
     while True:
-        pattern: list = input('Введите характеристики для поиска через пробел: ').split()
+        pattern: list = input(Fore.LIGHTGREEN_EX + 'Введите характеристики для поиска через пробел: ').split()
         if not pattern:
             os.system("clear||cls")
-            print("Не предоставлено ни одной характеристики для поиска")
+            print(Fore.LIGHTRED_EX + "Не предоставлено ни одной характеристики для поиска")
         else:
             break
     count = 0
@@ -145,7 +149,7 @@ def change_contact_information() -> None:
                     break
     if not count:
         print('В списке контактов нет человека с такими данными.')
-    print(separator)
+    print(Fore.LIGHTCYAN_EX + separator)
     telephone_book()
 
 
@@ -154,17 +158,17 @@ def telephone_book():
     A function to process user input and send it to the appropriate function
     :return: None
     """
-    print(f"{separator}\n{commands}\n{separator}\n\n")
+    print(Fore.GREEN + "Телефонный справочник".center(os.get_terminal_size().columns), end='\n\n')
+    print(Fore.LIGHTCYAN_EX + f"{separator}\n{commands}\n{separator}\n\n")
     while True:
         try:
-            action: int = int(input("####phone_book$$ "))
-            print()
+            action: int = int(input(Fore.CYAN + "####phone_book$$ "))
         except ValueError:
-            print('Неверные входные данные')
+            print(Fore.LIGHTRED_EX + 'Неверные входные данные')
         else:
             match action:
                 case 0:
-                    sys.exit(print("Всего доброго!".center(os.get_terminal_size().columns)))
+                    sys.exit(print(Fore.LIGHTGREEN_EX + "Всего доброго!".center(os.get_terminal_size().columns)))
                 case 1:
                     show_all_contacts()
                 case 2:
@@ -174,10 +178,9 @@ def telephone_book():
                 case 4:
                     search_contact()
                 case _:
-                    print('Неверные входные данные.')
+                    print(Fore.LIGHTRED_EX + 'Необходимо ввести целое число')
 
 
 if __name__ == "__main__":
     os.system("clear||cls")
-    print("Добро пожаловать в телефонный справочник.".center(os.get_terminal_size().columns), end='\n\n')
     telephone_book()
